d68a6f9ddb801b5768cf344604346c83bcbe5a6f
teena92084 <103328068+teena92084@users.noreply.github.com>
2022-06-22 09:54:29 +0530

    Add files via upload

---
+++ D,meeraki.q.6.py
@@ -0,0 +1,15 @@
+# person={
+# 'name':'jack',
+# 'age':20,
+# 'gender':'male',
+# 4:'organisation'}
+
+# add=person["age"]
+# x=person["name"]
+# y=person.get("gender")
+# print(y)
+# print(x)
+# print(add)
+# print(person[4])
+
+
